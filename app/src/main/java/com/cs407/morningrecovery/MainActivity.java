9f78721c903575ac6b17f6f8c9f52b44e810b740
Henry Ok <hwanok1020@gmail.com>
2023-11-01 16:42:19 -0500

    init

--- app/src/main/java/com/cs407/morningrecovery/MainActivity.java
+++ app/src/main/java/com/cs407/morningrecovery/MainActivity.java
@@ -3,6 +3,7 @@ package com.cs407.morningrecovery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+        Log.v("tag", "m");
     }
 }
