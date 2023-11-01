a4ed7862d15d917e736a410569a19ae71ed0569b
Jinwoong Shin <97860391+w0-0n9@users.noreply.github.com>
2023-11-01 18:21:16 -0500

    add custom button connection

---
+++ app/src/main/java/com/cs407/morningrecovery/CustomQuizActivity.java
@@ -0,0 +1,13 @@
+package com.cs407.morningrecovery;
+
+import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class CustomQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customquiz); // customquiz.xml 레이아웃을 사용합니다.
+    }
+}
--- app/src/main/java/com/cs407/morningrecovery/MainActivity.java
+++ app/src/main/java/com/cs407/morningrecovery/MainActivity.java
@@ -2,13 +2,25 @@ package com.cs407.morningrecovery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setContentView(R.layout.quizlevelselect);
+
+        Button customButton = this.<Button>findViewById(R.id.custombutton);
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomQuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
